cebe9a9c6c066394aeebfd35bf5de64b5676a40c
Will <will@kindleman.com.au>
2025-12-12 17:00:09 +0000

    add staging config code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  host = ENV['IS_STAGING'] ? 'example-staging.herokuapp.com' : 'example.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
